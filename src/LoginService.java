05865c8f236970f8dacbd88563eb1c279efeeaee
Romeo <rommar@jpj.com>
2020-08-13 14:22:49 +0800

    Added default method login(username, password, twoFactorCode)

--- src/LoginService.java
+++ src/LoginService.java
@@ -2,4 +2,8 @@ public interface LoginService {
 
     String login(String username, String password);
 
+    default String login(String username, String password, String twoFactorCode) {
+        return null;
+    }
+
 }
