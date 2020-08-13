9ea3b042417b8fc2f5587c1d2d23fce85e1e00f7
Romeo <rommar@jpj.com>
2020-08-13 19:11:17 +0800

    Modified LoginService to include a static method

--- src/LoginService.java
+++ src/LoginService.java
@@ -2,4 +2,15 @@ public interface LoginService {
 
     String login(String username, String password);
 
+    static boolean passwordsMatched(String password1, String password2) {
+        if (isBlank(password1) || isBlank(password2)) {
+            return false;
+        }
+        return password1.equals(password2);
+    }
+
+    static boolean isBlank(String password) {
+        return password == null || password.isEmpty();
+    }
+
 }
--- src/LoginServiceImpl.java
+++ src/LoginServiceImpl.java
@@ -12,7 +12,7 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public String login(String username, String password) {
         User user = userGateway.getUser(username);
-        if (user.getPassword().equals(password)) {
+        if (LoginService.passwordsMatched(user.getPassword(), password)) {
             return tokenGenerator.generateToken();
         }
         return null;
